df9dc7e55cd4d3855fb52fcd27f865c7c960363c
Jasczurka <matvey.senin@gmail.com>
2023-06-30 00:41:50 +0700

    Pt_2_4: Full task
    I wrote a program to solve the quadratic equation ax2 + bx + c = 0.
    The values of the coefficients a, b, c are entered from the keyboard.

---
+++ Pt_2_4.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+a, b, c = map(int, input("Введите коэффиценты квадратного уравнения: ").split())
+d = b ** 2 - 4 * a * c
+if d < 0:
+    print("Корней нет")
+elif d == 0:
+    print("x =", -b / (2 * a))
+else:
+    print("x1 =", (-b + sqrt(d)) / (2 * a))
+    print("x2 =", (-b - sqrt(d)) / (2 * a))
