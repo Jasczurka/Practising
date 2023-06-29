b890a27bf04299227756b9b68fa20757f2b9acab
Jasczurka <matvey.senin@gmail.com>
2023-06-29 22:09:01 +0700

    Pt_1_8: Full task
    I wrote a program that asks the user for his name and age and then
    displays a welcome message in the format
    "Привет, {name}! Тебе уже {age} лет."

---
+++ Pt_1_8.py
@@ -0,0 +1,7 @@
+name = input("Введите ваше имя: ")
+age = input("Введите ваш возраст: ")
+print(f"Привет, {name}! Тебе уже {age} лет.")
+
+I wrote a program that asks the user for his name and age and then
+displays a welcome message in the format
+"Привет, {name}! Тебе уже {age} лет."
