df82c6ac90a10cbf6f8caffbb584751c77cd7c2a
Jasczurka <75011699+Jasczurka@users.noreply.github.com>
2023-06-29 22:17:15 +0700

    Update Pt_1_8.py

--- Pt_1_8.py
+++ Pt_1_8.py
@@ -1,7 +1,3 @@
 name = input("Введите ваше имя: ")
 age = input("Введите ваш возраст: ")
 print(f"Привет, {name}! Тебе уже {age} лет.")
-
-I wrote a program that asks the user for his name and age and then
-displays a welcome message in the format
-"Привет, {name}! Тебе уже {age} лет."
