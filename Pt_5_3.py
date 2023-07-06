52bfa93b3fbcc54d405406e8d5a3a877473a720c
Jasczurka <matvey.senin@gmail.com>
2023-07-07 01:18:16 +0700

    Pt_5_3: Full task
    I have written a program that works as follows:
    Using the file books.csv, the program prompts the user to enter the
    beginning and end year. It then displays all the books published in
    a given period of time. The program's behavior in case of an invalid
    request is provided for.

---
+++ Pt_5_3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv("books.csv", delimiter=",")
+begin, end = map(int, input("Введите начальный и конечный год через пробел: ").split())
+try:
+    res = df.loc[(df["Год выпуска"] >= begin) & (df["Год выпуска"] <= end)]
+    if res.empty:
+        raise Exception
+    print(res)
+except Exception:
+    print("В списке нет книг, выпущенных в заданный период")
