90a8b5901695b1052bef413c0cde159545eb149b
Jasczurka <matvey.senin@gmail.com>
2023-07-20 14:00:04 +0700

    Pep 8 change
    Changed task 2 according to the PEP 8

--- Pt_4_2.py
+++ Pt_4_2.py
@@ -1,3 +1,4 @@
 string = " " + input()
-palindromes = [string[i:j + 1] for i in range(1, len(string)) for j in range(i, len(string)) if string[i:j + 1] == string[j:i - 1:-1]]
+palindromes = [string[i:j + 1] for i in range(1, len(string)) for j in
+               range(i, len(string)) if string[i:j + 1] == string[j:i - 1:-1]]
 print(palindromes)
